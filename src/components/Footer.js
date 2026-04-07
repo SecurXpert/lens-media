13748d668b5a337142fda7fcda56db7fbf390097
Sai13-hub <your-email@example.com>
2026-04-07 12:35:27 +0530

    blogs buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,6 +20,7 @@ export default function Footer() {
     { name: "About", href: "/about" },
     { name: "Services", href: "/services" },
     { name: "Projects", href: "/projects" },
+    { name: "Blogs", href: "/blogs" },
     { name: "Contact", href: "/contact" },
   ];
 
